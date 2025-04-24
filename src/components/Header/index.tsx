9151a884c5a87198961d69cc4ed86c33fdd1b513
aggab.sahar1995 <aggab.sahar1995@gmail.com>
2025-04-24 18:07:00 +0100

    Revert "last version of design"
    This reverts commit 453e0766af4ee64c82006f102a386b8a5f1a04c5.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -248,7 +248,7 @@ const Header = () => {
                 <span className="block relative cursor-pointer w-5.5 h-5.5">
                   <span className="du-block absolute right-0 w-full h-full">
                     <span
-                      className={`block relative top-0 left-0 bg-dark rounded-sm w-0 h-0.5 my-1 ease-in-out duration-200 delay-[0] ${!navigationOpen && "!w-full delay-300"
+                      className={`block relative top-0 left-0 bg-dark rounded-sm w-0 h-0.5 my-1 ease-in-out duration-200 delay-0 ${!navigationOpen && "!w-full delay-300"
                         }`}
                     ></span>
                     <span
@@ -263,7 +263,7 @@ const Header = () => {
 
                   <span className="block absolute right-0 w-full h-full rotate-45">
                     <span
-                      className={`block bg-dark rounded-sm ease-in-out duration-200 delay-300 absolute left-2.5 top-0 w-0.5 h-full ${!navigationOpen && "!h-0 delay-[0] "
+                      className={`block bg-dark rounded-sm ease-in-out duration-200 delay-300 absolute left-2.5 top-0 w-0.5 h-full ${!navigationOpen && "!h-0 delay-0 "
                         }`}
                     ></span>
                     <span
